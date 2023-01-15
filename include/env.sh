e03723f6c41c8b9b053cfed947df49c2dc8d46ba
Nathan Baum <n@p12a.org.uk>
2023-01-15 04:39:19 +0000

    Changes so far

--- include/env.sh
+++ include/env.sh
@@ -1,26 +1,29 @@
-
-if [ "$N" == "" ]; then
-  echo $0: N is not set to chain number
-  exit 1
-fi
+N=${N-N}
 
 export N
 export CHAIN=chain$N
 export ADDRESS=127.0.1.$N
 export NODENAME=node$N
 export CUDOS_HOME=var/run/$CHAIN
-export ACUDOS=acudos
-export FCUDOS=000$ACUDOS
-export PCUDOS=000$FCUDOS
-export NCUDOS=000$PCUDOS
-export UCUDOS=000$NCUDOS
-export MCUDOS=000$UCUDOS
-export CUDOS=000$MCUDOS
+export CUDOS_NODE=http://127.0.1.$N:26657
+export aCUDOS=acudos
+export fCUDOS=000$aCUDOS
+export pCUDOS=000$fCUDOS
+export nCUDOS=000$pCUDOS
+export uCUDOS=000$nCUDOS
+export mCUDOS=000$uCUDOS
+export CUDOS=000$mCUDOS
 export kCUDOS=000$CUDOS
 export MCUDOS=000$kCUDOS
 export GCUDOS=000$MCUDOS
 export TCUDOS=000$GCUDOS
-export PCUDOS=000$PCUDOS
+export PCUDOS=000$TCUDOS
+export ECUDOS=000$PCUDOS
+export ZCUDOS=000$ECUDOS
+export YCUDOS=000$ZCUDOS
+export RCUDOS=000$YCUDOS
+export QCUDOS=000$RCUDOS
+export SENDER=account0
 
 use-chain() {
   if [ "$1" == "" ]; then
@@ -28,22 +31,35 @@ use-chain() {
     return
   fi
   export N=$1
-  source include/variables.sh
+  source include/env.sh
+  ROOT=$(readlink -f $PWD/${BASH_SOURCE%/*}/..)
+  PATH=$ROOT/bin:$ROOT/lib:$PATH
+  echo configured $CHAIN
 }
 
 address() {
   cudos-noded keys show $1 -a --keyring-backend test
 }
 
-tx() {(
-  # set -o xtrace -e
+make-account() {
+  NAME=$1
+  shift
+  echo "$*" | cudos-noded keys add $NAME --keyring-backend test --recover --output json >/dev/null 2>&1
+  cudos-noded add-genesis-account $(address $NAME) 1$ECUDOS >/dev/null
+}
+
+query() {
+  cudos-noded query "$@" --node=$CUDOS_NODE --chain-id=$CHAIN
+}
+
+tx() {
   if ! [[ -z $SENDER ]]; then
-    cudos-noded tx "$@" --from $SENDER --keyring-backend=test --node=$CUDOS_NODE --chain-id=$CUDOS_CHAIN_ID --gas auto -y
+    cudos-noded tx "$@" --from $SENDER --keyring-backend=test --node=$CUDOS_NODE --chain-id=$CHAIN --gas auto --gas-prices=0acudos -y
   else
     echo usage: SENDER=... tx COMMAND... > /dev/stderr
     false
   fi
-)}
+}
 
 ibc-transfer() {
   local PORT=$1
@@ -51,7 +67,7 @@ ibc-transfer() {
   local TO=$3
   local AMOUNT=$4
   if ! [[ -z $PORT || -z $CHANNEL || -t $TO || -t $AMOUNT ]]; then
-    tx ibc-transfer transfer "$PORT" "$CHANNEL" "$TO" "$AMOUNT" 2>&1 >/dev/null
+    tx ibc-transfer transfer "$PORT" "$CHANNEL" "$TO" "$AMOUNT" >/dev/null
   else
     echo usage: ibc-transfer PORT CHANNEL TO AMOUNT > /dev/stderr
     false
@@ -80,7 +96,7 @@ wasm-instantiate() {
   if ! [[ -z $NAME || -z $CODE || -z $INIT ]]; then
     mkdir -p $CUDOS_HOME/instances
     CODE=$(cat $CUDOS_HOME/codes/$CODE)
-    tx wasm instantiate "$CODE" "$(cat $INIT | yq -j | jq -r tostring)" --label $NAME --admin $(account-address $SENDER) | \
+    tx wasm instantiate "$CODE" "$(cat $INIT | yq -j | jq -r tostring)" --label $NAME --admin $(address $SENDER) | \
       jq -r '.logs[].events[].attributes[]|select(.key=="_contract_address").value' | \
       sponge "$CUDOS_HOME/instances/$NAME"
     echo instantiated: instance $(cat $CUDOS_HOME/instances/$NAME) noted in "$CUDOS_HOME/instances/$NAME"
